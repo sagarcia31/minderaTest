4e15bd6c36d8b4fd9e2e0d52f1947a8a66e3cfc0
Marcelo Garcia <marcelo.sagarcia@cwi.com.br>
2019-02-24 20:38:01 -0300

    (#Mindera) - First commit

---
+++ mindera Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mindera Test
+//
+//  Created by Marcelo Garcia on 24/02/19.
+//  Copyright © 2019 Marcelo Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
